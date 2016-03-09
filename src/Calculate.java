4e2c5bdaf63cb56ceb9873d31119f890f5a8c02c
okuzkin <alexey.kuzkin@gmail.com>
2016-03-09 16:32:14 +0200

    Test commit

---
+++ src/Calculate.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Calculate {
+    public static void main(String[] args) throws IOException{
+        int first = Integer.valueOf(args[0]);
+        int second =Integer.valueOf(args[1]);
+        int sum= first+ second;
+        System.out.println(sum);
+
+
+    }
+}
